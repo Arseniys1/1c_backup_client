e640c4eec2ce1cd0873013df3a48c6c5745a32af
Arseniys1 <thevalakas1@gmail.com>
2022-06-08 20:22:11 +0500

    Конфиг времени бэкапов, concurrency, конфиг

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from config import config, ROOT_DIR, CONFIG_PATH, CONFIGS_PATH, LOGS_PATH, configs, configs_dirs
 from log import logger
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import datetime
 
 
 logger.info("Найдены директории с файлами конфигурации: " + ",".join(configs_dirs))
@@ -9,7 +11,13 @@ for config_obj in configs:
 
 
 def main():
-    pass
+    while True:
+        now = datetime.datetime.now()
+        now_hours_minutes = now.strftime("%H:%M")
+        for _config in configs:
+            for time in _config.files["time.txt"]:
+                if now_hours_minutes == time:
+                    pass
 
 
 if __name__ == '__main__':
